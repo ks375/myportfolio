bef7a8f34e90a4524180b6c22dbba794f3da5f5c
ks375 <chr1s.hunter0415@gmail.com>
2023-02-28 19:13:23 +0200

    change git link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/ks375/myportfolio'
       className='link footer__link'
     >
       Created By James Zhang
